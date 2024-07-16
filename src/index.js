4d1b242c47638d7dffdfc51a47f7a9f35b253ad0
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-07-16 07:43:53 +0545

    refactor our workout usning controller methods

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const v1Router = require("./v1/routes");
+// const v1Router = require("./v1/routes");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// remove
 // app.get("/", (req, res) => {
 //     res.send("Hello World");
 // });
 
-app.use("/api/v1", v1Router);
+// app.use("/api/v1", v1Router);
+app.use("/api/vi/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`API is listening on http://localhost:${PORT}`);
