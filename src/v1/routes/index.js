528ff4c13386b1a8e1d0747234059f4bab9afc70
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-07-16 07:26:28 +0545

    define endpoints inside workout router

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send(`<h2>Hello from ${res.baseUrl}</h2>`);
+    res.send("Get all workouts");
+});
+router.get("/:workoutId", (req, res) => {
+    res.send("Get an existing workout");
+});
+router.post("/", (req, res) => {
+    res.send("Create a new workout");
+});
+router.patch("/:workoutId", (req, res) => {
+    res.send("Update an existing workout");
+});
+router.delete("/:workoutId", (req, res) => {
+    res.send("Delete an existing workout");
 });
 
 module.exports = router;
