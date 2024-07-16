a87a5936e1808f201e593cfd9ed0ba2c09ef9e88
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-07-16 06:53:21 +0545

    spin up a router and hook up root entry point

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// for testing purposes
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+// remove
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
+
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
-    console.log(`API is listening on port ${PORT}`);
+    console.log(`API is listening on http://localhost:${PORT}`);
 });
